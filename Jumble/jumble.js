5bd8a65cfd8a94739451aa5a0fb450412b2f39ae
Jared Chua <jchua@albany.edu>
2019-04-29 20:44:34 -0400

    added some text, need to implement functionality

--- Jumble/jumble.js
+++ Jumble/jumble.js
@@ -1,31 +1,34 @@
+picklevel();
 
-var fs = require('fs');
-var data="";
-let counter=0;
-fs.readFile('C:/Users/jared/Desktop/Project/GameWebApplication/Jumble/levels/12.txt', 'utf8',function(err, data) {
-f
-    var doublearray = new Array();
-    for(let x=0; x<data.length; x++){
-        let array = new Array();
-        
-        let trigger=false;
-        while(data.charAt(x)!='\n'){    
-            let sb = "";
-            if(trigger){
-                x++;
+function picklevel(){
+    var fs = require('fs');
+    var data="";
+    let counter=0;
+    fs.readFile('C:/Users/jared/Desktop/Project/GameWebApplication/Jumble/levels/12.txt', 'utf8',function(err, data) {
+
+        var doublearray = new Array();
+        for(let x=0; x<data.length; x++){
+            let array = new Array();
+            
+            let trigger=false;
+            while(data.charAt(x)!='\n'){    
+                let sb = "";
+                if(trigger){
+                    x++;
+                }
+                while(data.charAt(x)!=' ' && data.charAt(x)!='\n'){
+                    sb += data.charAt(x);
+                    x++;
+                    trigger=true;
+                    
+                }
+                if(data.charAt(x)!='\n'){
+                    array.push(sb);
+                }
             }
-            while(data.charAt(x)!=' ' && data.charAt(x)!='\n'){
-                sb += data.charAt(x);
-                x++;
-                trigger=true;
-                
-            }
-            if(data.charAt(x)!='\n'){
-                array.push(sb);
-            }
+            
+            doublearray.push(array);
         }
-        
-        doublearray.push(array);
-    }
-    console.log(doublearray);
-});
+        console.log(doublearray);
+    });
+}
