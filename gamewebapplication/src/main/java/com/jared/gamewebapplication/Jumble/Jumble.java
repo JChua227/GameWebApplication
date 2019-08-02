d39a7648e1cb3f3f2f7634c675d1e7474813a677
JChua227 <jchua@albany.edu>
2019-08-01 22:13:15 -0400

    added domains for CORS

--- gamewebapplication/src/main/java/com/jared/gamewebapplication/Jumble/Jumble.java
+++ gamewebapplication/src/main/java/com/jared/gamewebapplication/Jumble/Jumble.java
@@ -2,11 +2,10 @@ package com.jared.gamewebapplication.Jumble;
 
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/level")
 public class Jumble {
-
 
     @GetMapping("/{levelnum}")
     public level getWordSet(@PathVariable(value="levelnum")int levelNumber){
