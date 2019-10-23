dee5c95d4479c767a3fe13947fdd5bbc7cde1ac0
Jared Chua <jchua@albany.edu>
2019-10-23 10:03:02 -0400

    properly organized imports

--- AngularGameWebApplication/src/app/app.module.ts
+++ AngularGameWebApplication/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TictactoeComponent } from './childComponents/tictactoe/tictactoe.component';
 import { NavbarComponent } from './childComponents/navbar/navbar.component';
 import { JumbleComponent } from './childComponents/jumble/jumble.component';
-import { HttpClientModule } from '@angular/common/http';
 import { TicTacToeComponent } from './parentComponents/tic-tac-toe/tic-tac-toe.component';
 import { ParentJumbleComponent } from './parentComponents/parent-jumble/parent-jumble.component';
 import { HomeComponent } from './parentComponents/home/home.component';
